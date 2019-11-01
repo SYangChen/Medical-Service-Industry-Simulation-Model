5446cbe9b12dd5b6d9886f1f6b2a62981246f264
Sophie(Ubuntu) <sophiexin036@gmail.com>
2019-11-01 15:20:05 +0800

    add image

--- cvs.py
+++ cvs.py
@@ -5,13 +5,11 @@ import time
 
 ### initialization ###
 
-d = 10 # diameter
-W = 1200;	  H = 800 # weight, height
-Wnum = int(W / d);  Hnum = int(H / d)
+d = 20 # rectangle size
+W, H = 1200, 800; # weight, height
+Wnum, Hnum = int(W / d), int(H / d)
 sleep_sec = 0.001
 DIRECTION = 8
-x_mv = [-d, 0, d, d, d, 0, -d, -d]
-y_mv = [d, d, d, 0, -d, -d, -d, 0]
 x_ele = [-1, 0, 1, 1, 1, 0, -1, -1]
 y_ele = [1, 1, 1, 0, -1, -1, -1, 0]
 world = []
@@ -24,8 +22,13 @@ window.title('group2_project')
 window.geometry(str(W)+'x'+str(H)) # '1200x800'
 #window.resizable(0,0) # fixed window size
 
+### load image ###
+normal_img = PhotoImage(file='./img/20x20/healthy.png')
+infected_img = PhotoImage(file='./img/20x20/blackVrs.png')
+doctor_img = PhotoImage(file='./img/20x20/syringe.png')
+
 ### canvas ###
-canvas = Canvas(window, bg='white', height=800, width=800)
+canvas = Canvas(window, bg='white', height=H, width=W)
 canvas.pack()
 
 def main():
@@ -35,6 +38,7 @@ def main():
 	turn = 0
 
 	### start life game ###
+	printMsg()
 	while(True):
 		world = generate(world, turn)
 		world = rule(world)
@@ -48,7 +52,7 @@ def main():
 def generate(world, turn):
 
 	### randomly generate patient ###
-	num_Patient = random.randint(10,20)
+	num_Patient = random.randint(5,20)
 	patient_cntr = 0
 	while(patient_cntr < num_Patient):
 		x = random.randint(0, Hnum-1)
@@ -59,10 +63,10 @@ def generate(world, turn):
 		else:
 			continue
 
-	### randomly generate patient every third turn ###
-	if(turn % 3 == 0):
+	### randomly generate doctor every third turn ###
+	if(turn % 2 == 0):
 		turn = 0
-		number_Doctor = random.randint(1,4) # 1~4
+		number_Doctor = random.randint(1, 5)
 		doctor_cntr = 0;
 		while(doctor_cntr < number_Doctor):
 			x = random.randint(0, Hnum-1)
@@ -82,6 +86,14 @@ def generate(world, turn):
 
 	return world
 
+def printMsg():
+	msg = Label(window, text="Hello World!", bg='purple', fg='white', font=('Arial', 12), width=30, height=2)
+	# 說明： bg為背景，fg為字型顏色，font為字型，width為長，height為高，這裡的長和高是字元的長和高，比如height=2,就是標籤有2個字元這麼高
+	msg.pack()
+
+	btn = Button(window, text='start', font=('Arial', 12), width=10, height=1)
+	btn.pack()
+
 def showWorld(world):
 
 	### draw canvas ###
@@ -91,14 +103,18 @@ def showWorld(world):
 		for j in range(Wnum):
 			x = d * j
 			if(world[i][j] == ' '):
-				canvas.create_oval(x, y, x+d, y+d, fill='white')
+				#canvas.create_rectangle(x, y, x+d, y+d, fill='white')
+				canvas.create_image(x, y, anchor='nw', image=normal_img)
 			elif(world[i][j] == '+'):
-				canvas.create_oval(x, y, x+d, y+d, fill='red')
+				#canvas.create_rectangle(x, y, x+d, y+d, fill='red')
+				canvas.create_image(x, y, anchor='nw', image=doctor_img)
 			else:
-				canvas.create_oval(x, y, x+d, y+d, fill='blue')
+				#canvas.create_rectangle(x, y, x+d, y+d, fill='blue')
+				canvas.create_image(x, y, anchor='nw', image=infected_img)
 		x %= W
 
 	### show on terminal ###
+	"""
 	print("--"*62)
 	for i in range(Hnum):
 		print("|", end = "")
@@ -106,6 +122,7 @@ def showWorld(world):
 			print(world[i][j], end="")
 		print(" |")
 	print("--"*62, end = "\n")
+	"""
 
 def countPeople(world):
 	normal = 0  # number of normal
@@ -127,6 +144,7 @@ def rule(world):
 	c = countPeople(world)
 
 	### Doctor is oversupply ###
+	"""
 	if( (c['patient'] != 0) and (c['doctor'] / c['patient']) > 3 ):
 		number_Doctor = random.randint(1, 3) # retire
 		doctor_cntr = 0
@@ -136,6 +154,7 @@ def rule(world):
 			if(world[x][y] == '+'):
 				world[x][y] = ' '
 				doctor_cntr += 1
+	"""
 
 
 	for i in range(Hnum):
@@ -153,7 +172,7 @@ def rule(world):
 			if(world[i][j] == 'X' or world[i][j] == ' '):
 				###  Cluster infection ###
 				if(patient_cntr >= 5):
-					infected = random.randint(1,4)
+					infected = random.randint(0,3)
 					for n in range(infected):
 						d = random.randint(0,7); # give random direction
 						if(not overBorder(i + x_ele[d],j + y_ele[d])):
@@ -161,6 +180,14 @@ def rule(world):
 
 			### Doctor case ##
 			else:
+				### count patient in 8 direction ###
+				patient_cntr = 0
+				for d in range(DIRECTION):
+					_x = i + x_ele[d]
+					_y = j + y_ele[d]
+					if(not overBorder(_x,_y) and world[_x][_y] == 'X'):
+						patient_cntr += 1
+
 				###  Healing patient(s) ###
 				if(patient_cntr < 6):
 					for d in range(DIRECTION):
@@ -169,8 +196,8 @@ def rule(world):
 					pos_i, pos_j = random.randint(-1,1), random.randint(-1,1)
 					if(not overBorder(i+pos_i, j+pos_j)):
 						world[i][j] = ' '
-						i, j = i+pos_i, j+pos_j
-						world[i][j] = '+'
+						world[i+pos_i][j+pos_j] = '+'
+
 				### Doctor Overworked or retired ###
 				else:
 					world[i][j] = ' '
