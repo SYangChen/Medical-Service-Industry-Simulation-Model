4227767276dcb5dd259d93893f90a671bf188249
SophieXin <sophiexin036@gmail.com>
2019-11-03 13:53:07 +0800

    debug

--- autorun.sh
+++ autorun.sh
@@ -9,13 +9,14 @@ for i in {1..10};
 do
 	current_usage = 0.0
 
-	while ["$current_usage" -le $CPU_usage]
+	while ["$current_usage" -le "$CPU_usage"]
 	do
 		current_usage=$(top -bn2 | grep "python3" | awk '{print $9}' | python3 -c "print(input())")
-		echo ${current_usage}
+		#echo ${current_usage}
 	done
 
 	sudo kill -9 $(ps -ef | grep "python3 main.py" | awk  '{print $2}' | python3 -c "print(input())")
 	echo "Kill main.py"
+	sleep 5
 	exec python3 main.py &
 done
